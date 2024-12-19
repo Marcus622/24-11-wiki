caf1ad33e4e55345b4aa541bf3e898400447937a
Marcus622 <marcus.vix@tn.techstarter.de>
2024-12-19 14:38:47 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,9 @@
+first_name = "Marcus"
+last_name = "V."
+print (first_name+" "+last_name)
+
+zahl1 = 1
+zahl2 = 10
+print("Zahl 1 + Zahl 2:", zahl1 + zahl2)
+print("Zahl 1 * Zahl 2:", zahl1*zahl2)
+print("Zahl 1 / Zahl 2:", zahl1 / zahl2)           
